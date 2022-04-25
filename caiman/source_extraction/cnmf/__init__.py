5259e1bd39feacf4c9607e86c72777662634b0b9
Kabilar Gunalan <kabilar@datajoint.com>
2022-04-24 22:52:30 -0500

    Remove import

--- caiman/source_extraction/cnmf/__init__.py
+++ caiman/source_extraction/cnmf/__init__.py
@@ -9,7 +9,6 @@ from . import merging
 from . import pre_processing
 from . import spatial
 from . import temporal
-from . import oasis
 from . import params
 from . import online_cnmf
 from .cnmf import CNMF as CNMF
